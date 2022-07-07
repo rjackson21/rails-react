a746538b3c7c1e2d10234927d5583fa86f8e27dd
Regina Jackson <rjackson156@gmail.com>
2022-07-06 21:17:16 -0400

    fix syntax

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import ".react/src/index.js" //entry point for the build in package.json
+import "./react/src/index.js" //entry point for the build in package.json
